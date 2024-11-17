9a60f3ed4c1a3cb40c2a0fd7328b95bcc878adf4
Eren Akgün <iobthedev@outlook.com>
2024-11-17 12:15:13 +0300

    feat!: add experimental referencing
    Currently breaks old databases. A migration utility will be created.

--- sozluk/entry.py
+++ sozluk/entry.py
@@ -1,16 +1,73 @@
 import dataclasses
+import logging
+import re
 from datetime import datetime, timedelta
 from typing import Type, TypeVar
+
+from markupsafe import escape
 
 from sozluk.authorname import AuthorName
 from sozluk.topicname import TopicName
 from sozluk.turkishlowercasedstring import TurkishLowercasedString
 
+logger = logging.getLogger(__name__)
+
 T = TypeVar("T", bound="EntrySketch")
+
+ENTRY_REFERENCE_PARSER = {
+    "(bkz: {})": re.compile(r"\(bkz: ((?:[#@]?[a-z \d])+)?\)"),
+    "(ayrica bkz: {})": re.compile(r"\(ayrica bkz: ((?:[#@]?[a-z \d])+)?\)"),
+    "(ayrıca bkz: {})": re.compile(r"\(ayrıca bkz: ((?:[#@]?[a-z \d])+)?\)"),
+    "{}": re.compile(r"`([#@]?[a-z \d]+)?`"),
+}
 
 
 class EntryText(TurkishLowercasedString):
-    pass
+
+    def parse(self):
+        # escape for python format syntax
+        string = self.replace("{", "{{").replace("}", "}}")
+
+        indexes = {}
+
+        for replacement, parser in ENTRY_REFERENCE_PARSER.items():
+            for m in parser.finditer(string):
+                indexes[m.span()[0]] = m.group(1)
+
+        matches = [v for k, v in sorted(indexes.items(), key=lambda item: item[0])]
+
+        for replacement, parser in ENTRY_REFERENCE_PARSER.items():
+            string = parser.sub(replacement, string)
+
+        return string, matches
+
+    def render(self, topic_prefix="", entry_prefix="", author_prefix=""):
+        try:
+            string, matches = self.parse()
+        except Exception as e:
+            logger.exception(e)
+            return self
+
+        string = str(escape(string))
+
+        formatted = []
+        for i in matches:
+            if i.startswith("@"):
+                formatted.append(
+                    f"<a href='{author_prefix}{i.removeprefix('@')}'>{i}</a>"
+                )
+            elif i.startswith("#"):
+                formatted.append(
+                    f"<a href='{entry_prefix}{i.removeprefix('#')}'>{i}</a>"
+                )
+            else:
+                formatted.append(f"<a href='{topic_prefix}{i}'>{i}</a>")
+
+        try:
+            return string.format(*formatted)
+        except Exception as e:
+            logger.exception(e)
+            return self
 
 
 @dataclasses.dataclass
