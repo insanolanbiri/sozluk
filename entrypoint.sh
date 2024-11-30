a395b8cd43ed0b6b64220df578b8cfa4610ef581
Eren Akgün <iobthedev@outlook.com>
2024-11-30 14:02:04 +0300

    fix: do not use gevent

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,4 @@
 
 set -e
 
-exec .venv/bin/gunicorn -k gevent sozluk.app:app -b 0.0.0.0:8080 -w $((${THREADS:-$(nproc)}))
+exec .venv/bin/gunicorn sozluk.app:app -b 0.0.0.0:8080 -w $((${THREADS:-$(nproc)}))
