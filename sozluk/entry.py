20bd3d72c1a68325522fc3d3eadb2db08fcab217
Eren Akgün <iobthedev@outlook.com>
2024-12-07 10:16:20 +0300

    fix!(EntryText): replace CRLF with LF

--- sozluk/entry.py
+++ sozluk/entry.py
@@ -28,6 +28,7 @@ class EntryText(TurkishLowercasedString):
     def __new__(cls, o: object = ""):
         o = str(o)
         o = o.lstrip().rstrip()
+        o = o.replace("\r\n", "\n")
         if emoji_count(o) != 0:
             raise ValueError("emojis in an entry text is not allowed")
         return super().__new__(cls, o)
